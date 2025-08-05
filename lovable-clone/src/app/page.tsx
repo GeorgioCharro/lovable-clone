2f598a4f48df6df29c0c5f59acd9391f3ae9975e
GeorgioCharro <georgiocharro@hotmail.com>
2025-08-05 13:05:08 +0300

    [Web] Added boilerpoint files / TRPC for api calls / ShadCN / [ Server] Integrated prisma with the project + NeoDB with postgres

---
+++ lovable-clone/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Home() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Gio" }));
+  return (
+    
+    <div className="">
+      {JSON.stringify(data)}
+    </div>
+  );
+}
---
+++ lovable-clone/src/trpc/routers/_app.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+export const appRouter = createTRPCRouter({
+    hello: baseProcedure
+        .input(
+            z.object({
+                text: z.string(),
+            })      
+        )
+        .query(({ input }) => {
+            return {
+                greeting: `Hello ${input.text ?? 'world'}`,
+            };
+        }),
+    });
+
+    export type AppRouter = typeof appRouter;
