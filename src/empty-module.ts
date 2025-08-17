9e2bd0d740e42eb82ecb4cc4399dd2b4d3551a4e
GeorgioCharro <georgiocharro@hotmail.com>
2025-08-17 19:40:06 +0300

    feat: add Hook class to empty module and export it alongside an empty object

--- src/empty-module.ts
+++ src/empty-module.ts
@@ -1,1 +1,7 @@
-export {};
+export class Hook {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  constructor(..._args: unknown[]) {}
+}
+
+export const empty = {};
+export default empty;
